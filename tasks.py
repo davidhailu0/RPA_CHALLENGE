d6a0a4bdc74b445dec6371deddeae4bd669bee95
Dawit Hailu <davidhailu0@gmail.com>
2024-04-16 22:07:49 +0300

    fixed some of the issues

--- tasks.py
+++ tasks.py
@@ -5,8 +5,7 @@ from robocorp import workitems
 def minimal_task():
     item = workitems.inputs.current
     value = "Science"
-    if item.payload!=None and list(item.payload.keys())[0]!='' and item.payload[list(item.payload.keys())[0]]!='':
+    if item.payload!=None and len(item.payload)!=0 and list(item.payload.keys())[0]!='' and item.payload[list(item.payload.keys())[0]]!='':
         value = item.payload[list(item.payload.keys())[0]]
-    print(value)
     al = AljazeeraExtractor()
     al.extractLatestNews(value)
