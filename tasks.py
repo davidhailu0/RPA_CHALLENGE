096198602ff524499e58574345bcad7ee1b4c31d
Dawit Hailu <davidhailu0@gmail.com>
2024-04-16 21:52:11 +0300

    added validation

--- tasks.py
+++ tasks.py
@@ -4,5 +4,9 @@ from robocorp import workitems
 @task
 def minimal_task():
     item = workitems.inputs.current
+    value = "Science"
+    if item.payload!=None and list(item.payload.keys())[0]!='' and item.payload[list(item.payload.keys())[0]]!='':
+        value = item.payload[list(item.payload.keys())[0]]
+    print(value)
     al = AljazeeraExtractor()
-    al.extractLatestNews(item.payload)
+    al.extractLatestNews(value)
