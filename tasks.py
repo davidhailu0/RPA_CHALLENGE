e95d5a474af580d1a96171fbc5f69964a9a86160
Dawit Hailu <davidhailu0@gmail.com>
2024-04-18 19:39:33 +0300

    refactored the code

--- tasks.py
+++ tasks.py
@@ -1,11 +1,13 @@
 from robocorp.tasks import task
 from AlijazeeraExtractor import AljazeeraExtractor
-from robocorp import workitems
+from RPA.Robocorp.WorkItems import WorkItems
 @task
 def minimal_task():
-    item = workitems.inputs.current
+    item = WorkItems()
+    # payload = item.get_work_item_payload()
+    # print(payload)
     value = "Science"
-    if item.payload!=None and len(item.payload)!=0 and list(item.payload.keys())[0]!='' and item.payload[list(item.payload.keys())[0]]!='':
-        value = item.payload[list(item.payload.keys())[0]]
+    # if item.payload!=None and len(item.payload)!=0 and list(item.payload.keys())[0]!='' and item.payload[list(item.payload.keys())[0]]!='':
+    #     value = item.payload[list(item.payload.keys())[0]]
     al = AljazeeraExtractor()
-    al.extractLatestNews(value)
+    al.extract_latest_news(value)
